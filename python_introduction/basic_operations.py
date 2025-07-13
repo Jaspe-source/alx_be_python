cac6b87b80873dc57f5558f52753307d0ef03f6c
Jasper Otieno <jasperotieno1@gmail.com>
2025-07-13 14:30:25 +0300

    Add basic arithmetic script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+
+diff_result = number1 - number2
+print(f"Substraction of {number1} and {number2} is {diff_result}")
+
+product_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
